a986c5dc1990bb11e312877364ca941dca6fd0e1
Haylton Santos <aylton.souza10@gmail.com>
2025-09-04 14:41:52 +0100

    106 listviwew builder

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -3,64 +3,58 @@ import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
-  
   final List<Transaction> transaction;
 
   TransactionList(this.transaction);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      height: 300, //onrigatorio para ter scroll e o ListView puro cria todas as estancias ao mesmo tempo temos q eu usar o builder 
-      child: ListView(
+    return Container(
+      height:
+          300, //onrigatorio para ter scroll e o ListView puro cria todas as estancias ao mesmo tempo temos q eu usar o builder
+      child: ListView.builder(
+        itemCount: transaction.length, //tamanho da lista em qnt
+        itemBuilder: (ctx, index) { //constroi a lista conforme a necessidade
+          final tr = transaction[index];
+          return Card(
+            child: Row(
               children: <Widget>[
-                ...transaction.map((tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: EdgeInsets.symmetric(
-                          horizontal: 15,
-                          vertical: 10
-                        ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.black,
-                            width: 2
-                          )
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple
-                          ),
-                        ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(tr.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          ),
-                          Text(DateFormat('d MMM y').format(tr.date),
-                          style: TextStyle(
-                            color: Colors.grey[500]
-                          ),
-                          )
-                        ],
-                      )
-                    ],
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 2),
                   ),
-                );
-              }).toList()
-              ]
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tr.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat('d MMM y').format(tr.date),
+                      style: TextStyle(color: Colors.grey[500]),
+                    ),
+                  ],
+                ),
+              ],
             ),
+          );
+        },
+      ),
     );
   }
 }
