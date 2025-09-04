642c9997396a5e76d47858502905cadc10b45525
Haylton Santos <aylton.souza10@gmail.com>
2025-09-04 14:35:04 +0100

    105 - adicionando scroll nas telas

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -5,48 +5,54 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
   @override
   Widget build(BuildContext context) {
-    return Card(
-            elevation: 5,
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                children: <Widget>[
-                  
-                  TextField(
-                    controller: titleController,
-                    decoration: InputDecoration(
-                    labelText: 'Titulo'
+    return SingleChildScrollView(
+      child: Card(
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  children: <Widget>[
+                    
+                    TextField(
+                      controller: titleController,
+                      decoration: InputDecoration(
+                      labelText: 'Titulo'
+                      ),
                     ),
-                  ),
-
-                  TextField(
-                    controller: valueController,
-                    decoration: InputDecoration(
-                    labelText: 'Valor R\$'
-                    )
-                  ),
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      FilledButton(
-                        onPressed: () {
-                          print(titleController.text);
-                          print(valueController.text);
-                        }, 
-                        child: Text('Nova Transação',
-                        style: TextStyle(
-                          color: Colors.white
+      
+                    TextField(
+                      controller: valueController,
+                      decoration: InputDecoration(
+                      labelText: 'Valor R\$'
+                      )
+                    ),
+      
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        FilledButton(
+                          onPressed: () {
+                            final title = titleController.text;
+                            final value = double.tryParse(valueController.text) ?? 0.0;
+                            onSubmit(title, value);
+                          }, 
+                          child: Text('Nova Transação',
+                          style: TextStyle(
+                            color: Colors.white
+                            ),
+                            )
                           ),
-                          )
-                        ),
-                    ],
-                  )
-                ],
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-          );
+    );
   }
 }
